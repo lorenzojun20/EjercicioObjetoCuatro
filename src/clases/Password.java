10d91587bda5d06125468114060f758c9e1f1cb3
Lorenzo Junior <elkena79@gmai.com>
2016-09-12 21:02:28 -0500

    Objeto Cuatro Terminado Completamente

--- src/clases/Password.java
+++ src/clases/Password.java
@@ -10,5 +10,41 @@ package clases;
  * @author Lorenzo
  */
 public class Password {
+    private int longitud;
+    private String clave;
     
+    public Password(int longi,String clave){
+        this.longitud = longi;
+        this.clave = clave;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    public String estabilidad(){
+        
+        
+        String clave;
+       
+        
+        if(this.longitud >= 6){
+            clave = "La contraseña es fuerte";
+        }else{
+            clave = "La contraseña es débil";
+        }
+        
+        return clave;
+    }
 }
